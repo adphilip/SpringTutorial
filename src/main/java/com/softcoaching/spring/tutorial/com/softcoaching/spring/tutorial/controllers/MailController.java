88264043d9639e477d5e439b648be53d5fd6c41b
Adrian Filipescu <adrian.filipescu@1and1.ro>
2016-03-25 17:48:08 +0200

    Mail content Injection.

--- src/main/java/com/softcoaching/spring/tutorial/com/softcoaching/spring/tutorial/controllers/MailController.java
+++ src/main/java/com/softcoaching/spring/tutorial/com/softcoaching/spring/tutorial/controllers/MailController.java
@@ -1,9 +1,10 @@
 package com.softcoaching.spring.tutorial.com.softcoaching.spring.tutorial.controllers;
 
 import com.softcoaching.spring.tutorial.com.softcoaching.spring.tutorial.mail.MailSender;
-import com.softcoaching.spring.tutorial.com.softcoaching.spring.tutorial.mail.MockMailSender;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by afilipescu on 3/25/16.
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MailController {
-    private MailSender mailSender = new MockMailSender();
+    @Resource
+    private MailSender mailSender;
+    /*classic way*/
+    //private MailSender mailSender = new MockMailSender();
 
     @RequestMapping("/mail")
-
     public String sendMail() {
         mailSender.send("adrian.filipescu@gmail.com", "De la mine", "the content");
         return "Mail sent";
--- src/main/java/com/softcoaching/spring/tutorial/com/softcoaching/spring/tutorial/mail/MockMailSender.java
+++ src/main/java/com/softcoaching/spring/tutorial/com/softcoaching/spring/tutorial/mail/MockMailSender.java
@@ -2,12 +2,12 @@ package com.softcoaching.spring.tutorial.com.softcoaching.spring.tutorial.mail;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by afilipescu on 3/25/16.
  */
-
-
+@Component
 public class MockMailSender implements MailSender {
     private static final Log log = LogFactory.getLog(MockMailSender.class);
 
