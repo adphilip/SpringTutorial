ca2252ee2c1c24d2a16409c20d1d4049b05deda0
Adrian Filipescu <adrian.filipescu@1and1.ro>
2016-03-28 12:09:30 +0300

    Application properties variables.

--- src/main/java/com/softcoaching/spring/tutorial/com/softcoaching/spring/tutorial/controllers/HelloController.java
+++ src/main/java/com/softcoaching/spring/tutorial/com/softcoaching/spring/tutorial/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.softcoaching.spring.tutorial.com.softcoaching.spring.tutorial.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+
+    @Value("${app.name}")
+    private String appName;
+
     @RequestMapping("/hello")
     public String hello () {
-        return "Hello , world from Hello Controller!";
+        return "Hello , world from Hello Controller! .... " + appName;
     }
 }
